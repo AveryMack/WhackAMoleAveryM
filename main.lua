383998a69e97724ae8f39c5154dda20c698a6531
Avery Mack <avery.mack@ocsbstudent.ca>
2018-04-04 20:22:10 -0400

    changes

--- main.lua
+++ main.lua
@@ -15,11 +15,17 @@
 -- hide the staus bar 
 display.setStatusBar(display.HiddenStatusBar)
 
+-- play background music
+local backgroundMusic = audio.loadSound("sounds/backgroundMusic.mp3")
+local backgroundMusicChannel
+backgroundMusicChannel = audio.play(backgroundMusic) 
+
+
 -- creating background
 local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight) 
 
 	-- setting color
-	bkg:setFillColor( 0.5, 1, 0 )
+	bkg:setFillColor( 0/255, 109/255, 0/255 )
 
 	-- setting position
 	bkg.anchorX = 0 
@@ -27,42 +33,42 @@ local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight)
 	bkg.x = 0 
 	bkg.y = 0 
 
--- creating mole
-local mole = display.newImage( "Images/mole.png" , 0, 0 )
+-- creating horse
+local horse = display.newImage( "Images/horse.png" , 0, 0 )
 
 	-- setting position
-	mole.x = display.contentCenterX
-	mole.y = display.contentCenterY
+	horse.x = display.contentCenterX
+	horse.y = display.contentCenterY
 
-	-- scale down the size of the mole to be a third of its original size
-	mole:scale( 0.3, 0.3 )
+	-- scale down the size of the horse to be a third of its original size
+	horse:scale( 0.95, 0.95 )
 
-	-- make the mole invisible
-	mole.isVisible = false 
+	-- make the horse invisible
+	horse.isVisible = false 
 
 -- add local variable to keep track of the score 
 local points = 0
 local pointsObject
 
 -- display a points object and set the colour
-pointsObject = display.newText( "0 ", 900, 700, nil, 100)
-pointsObject:setTextColor(255/255, 255/255, 255/255)
+pointsObject = display.newText( "0 ", 525, 50, nil, 75)
+pointsObject:setTextColor(155/255, 0/255, 131/255)
 
 --------------------------------Functions---------------------------------------------------------------
 
--- this function that makes a mole appear in a random (x,y) position on the screen
+-- this function that makes a horse appear in a random (x,y) position on the screen
 -- before calling the Hide function
 function PopUp()
 
 	-- choosing random position on the screen betweem 0 and the size of the screen 
-	mole.x = math.random( 0, display.contentWidth)
-	mole.y = math.random( 0, display.contentHeight)
+	horse.x = math.random( 0, display.contentWidth)
+	horse.y = math.random( 0, display.contentHeight)
 
-	-- make the mole visible 
-	mole.isVisible = true 
+	-- make the horse visible 
+	horse.isVisible = true 
 
 	-- call the hide function after 500 milliseconds 
-	timer.performWithDelay( 500, Hide)
+	timer.performWithDelay( 800, Hide)
 end 
 
 -- this function calls the pop up function after 3 seconds 
@@ -70,11 +76,11 @@ function PopUpDelay()
 	timer.performWithDelay( 3000, PopUp )
 end
 
--- this function makes the mole invisible and then calls the pop up delay function
+-- this function makes the horse invisible and then calls the pop up delay function
 function Hide()
  	
  	-- changing visibility 
- 	mole.isVisible = false 
+ 	horse.isVisible = false 
  	PopUpDelay()
  end 
 
@@ -89,14 +95,17 @@ function Hide()
  	-- if touch phase just started 
  	if (event.phase == "began") then
  		points = points + 1 
- 		pointsObject.text = points 
+ 		pointsObject.text = points
+ 		local whackSound = audio.loadSound("sounds/whack.mp3")
+ 		local whackSoundChannel
+ 		whackSoundChannel = audio.play(whackSound) 
  	end 
 
 end 
 
 ----------------------------------------------Event Listeners----------------------------------------------
--- I add the event listener to the moles so that if the mole is touched, the Whacked function is called
-mole:addEventListener( "touch", Whacked)
+-- I add the event listener to the moles so that if the horse is touched, the Whacked function is called
+horse:addEventListener( "touch", Whacked)
 
 -------------------------------------------Start the Game---------------------------------------------------
 -- call the function that starts the game 
